83d8f0e089611e62d5fd410a87da5917976595cb
huandt93 <huannguyen7651@gmail.com>
2022-10-01 11:45:36 +0700

    update

--- install_docker-centos.sh
+++ install_docker-centos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo yum update && sudo yum upgrade -y
+sudo yum install curl build-essential git wget jq make gcc ack tmux ncdu -y
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
-apt update && apt install git sudo unzip wget -y
+yum update && yum install git sudo unzip wget -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
